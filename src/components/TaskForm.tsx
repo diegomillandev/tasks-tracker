2c80b4893129d3b4455087a7df9331f08568b927
diegomillandev <millan@iMac-Pro.local>
2024-05-08 17:06:39 -0500

    delete script, flowbite

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -194,17 +194,6 @@ export const TaskForm = () => {
                         Date
                     </label>
                     <div className="relative">
-                        <div className="absolute inset-y-0 end-3 flex items-center ps-3.5 pointer-events-none">
-                            <svg
-                                className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                            >
-                                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                            </svg>
-                        </div>
                         <input
                             type="date"
                             className={`w-full p-2.5 mt-1 bg-[#1e2a36] focus:outline-none focus:ring-2
